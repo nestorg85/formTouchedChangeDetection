ea976186e12dfd2b8399af77683415fd01725df5
Nestor Guerrero <40645688+nestorg85@users.noreply.github.com>
2021-11-30 14:37:31 -0500

    Add files via upload

---
+++ reactive-form.utils.ts
@@ -0,0 +1,35 @@
+/**
+ * Extract arguments of function
+ */
+import {AbstractControl} from "@angular/forms";
+import {Observable, Subject} from "rxjs";
+
+type ArgumentsType<F> = F extends (...args: infer A) => any ? A : never;
+
+/**
+ * Creates an object like O. Optionally provide minimum set of properties P which the objects must share to conform
+ */
+type ObjectLike<O extends object, P extends keyof O = keyof O> = Pick<O, P>;
+
+
+export const touchedChanges = (control: ObjectLike<AbstractControl, 'markAsTouched' | 'markAsUntouched'>): Observable<boolean> => {
+  const prevMarkAsTouched = control.markAsTouched;
+  const prevMarkAsUntouched = control.markAsUntouched;
+
+  const touchedChanges$ = new Subject<boolean>();
+
+  function nextMarkAsTouched(...args: ArgumentsType<AbstractControl['markAsTouched']>) {
+    touchedChanges$.next(true);
+    prevMarkAsTouched.bind(control)(...args);
+  }
+
+  function nextMarkAsUntouched(...args: ArgumentsType<AbstractControl['markAsUntouched']>) {
+    touchedChanges$.next(false);
+    prevMarkAsUntouched.bind(control)(...args);
+  }
+
+  control.markAsTouched = nextMarkAsTouched;
+  control.markAsUntouched = nextMarkAsUntouched;
+
+  return touchedChanges$;
+}
